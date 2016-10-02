f3dfdcc27b1f254083844d2eb510e830c0f40bf0
Kevin Qiu <kevinqiu96@gmail.com>
2016-10-02 14:38:31 -0700

    stuff

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,4 +2,9 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("myApp", ["$scope", "$http", function($scope, $http) {
 	console.log("Hello world from controller");
+	
+	$http.get("/contactlist").success(function(response) {
+		console.log("I got the data I requested");
+		$scope.contactlist = response;
+	});
 }]);
--- server.js
+++ server.js
@@ -3,5 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/contactlist", function(req, res) {
+	console.log("I received a GET request");
+	
+	person1 = {
+		name: "Bob",
+		email: "bob@bob.com",
+		number: "123 123-1234"
+	};	
+	
+	person2 = {
+		name: "Jane",
+		email: "jane@jane.com",
+		number: "123 321-1234"
+	};
+	
+	person3 = {
+		name: "xdMan",
+		email: "xd@xd.com",
+		number: "123 321-6969"
+	};
+	
+	var contactlist = [person1, person2, person3];
+	
+	res.json(contactlist);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
